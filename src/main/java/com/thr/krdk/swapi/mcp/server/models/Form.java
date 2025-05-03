7075bc2272c44af9f9b6f86cadcc6e3f01aa7a0f
Tahir Kardak (ETIYA) <thrkardak@gmail.com>
2025-05-03 21:05:44 +0300

    - Yml Setting for MCP Server - Dockerfiles created for SSE and Stdio

--- src/main/java/com/thr/krdk/swapi/mcp/server/models/Form.java
+++
@@ -1,7 +0,0 @@
-package com.thr.krdk.swapi.mcp.server.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Form(long id, String formName, String description, long status) {
-}
