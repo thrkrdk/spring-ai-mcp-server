b31f5834706e6181f7eb1378cde1cd9e0327383d
Tahir Kardak (ETIYA) <thrkardak@gmail.com>
2025-05-03 21:38:50 +0300

    - Server Prompt Added. - Readme file updated - checkstyle fixes

--- src/main/java/com/thr/krdk/swapi/mcp/server/SwapiMcpStarterApplication.java
+++ src/main/java/com/thr/krdk/swapi/mcp/server/SwapiMcpStarterApplication.java
@@ -1,10 +1,14 @@
 package com.thr.krdk.swapi.mcp.server;
 
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SwapiMcpStarterApplication {
@@ -18,5 +22,41 @@ public class SwapiMcpStarterApplication {
         return MethodToolCallbackProvider.builder().toolObjects(swapiToolsService, calculatorToolsService).build();
     }
 
+    // prompt
+    @Bean
+    public List<McpServerFeatures.SyncPromptSpecification> starWarsCharacterPrompts() {
+        // Prompt description: Id, description and list of arguments
+        var prompt = new McpSchema.Prompt(
+                "starWarsCharacterById",                                  // prompt name
+                "Fetch a Star Wars character by ID using the SWAPI",      // prompt description
+                List.of(new McpSchema.PromptArgument(
+                        "characterId",                                        // name of the argument
+                        "ID of the Star Wars character to retrieve",          // description for the argument
+                        true                                                  // mandatory
+                ))
+        );
+
+        // Prompt handler: creates a user message based on the incoming character ID.:
+        var promptSpecification = new McpServerFeatures.SyncPromptSpecification(
+                prompt,
+                (exchange, getPromptRequest) -> {
+                    String id = (String) getPromptRequest.arguments().get("characterId");
+                    // Create a user message with the character ID. This is default prompt. Client will use this prompt to get the character details.
+                    var userMessage = new McpSchema.PromptMessage(
+                            McpSchema.Role.USER,
+                            new McpSchema.TextContent(String.format(
+                                    "Please return all details of the character with ID \"%s\" using the Star Wars API (SWAPI) — name, birth date, height, mass, list of films, etc. — in JSON format.",
+                                    id
+                            ))
+                    );
+                    return new McpSchema.GetPromptResult(
+                            "Retrieve detailed Star Wars character information in JSON format",
+                            List.of(userMessage)
+                    );
+                }
+        );
+
+        return List.of(promptSpecification);
+    }
 
 }
