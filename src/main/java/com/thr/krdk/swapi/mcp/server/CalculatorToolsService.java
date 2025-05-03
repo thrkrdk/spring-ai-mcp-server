0b1c6531694b7ff97218b96b743041145674b045
Tahir Kardak (ETIYA) <thrkardak@gmail.com>
2025-05-03 21:38:50 +0300

    - Server Prompt Added. - Readme file updated - checkstyle fixes

--- src/main/java/com/thr/krdk/swapi/mcp/server/CalculatorToolsService.java
+++ src/main/java/com/thr/krdk/swapi/mcp/server/CalculatorToolsService.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorToolsService {
+    private static final long MULTIPLIER = 10000000;
 
     @Tool(description = "Calculate the sum of two numbers")
     public long sum(@ToolParam(description = "First number") long numberA,
                     @ToolParam(description = "Second number") long numberB) {
-        return (numberA + numberB) * 10000000;
+        return (numberA + numberB) * MULTIPLIER;
     }
 }
