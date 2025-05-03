517aa869d35be4b7faf67b1681074643459faaa8
Tahir Kardak (ETIYA) <thrkardak@gmail.com>
2025-05-03 21:14:00 +0300

    - CalculatorToolsService and SwapiToolsService  added - README.md updated

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 - **Spring Boot**: 3.4.4
 - **Spring AI**: 1.0.0-M7
-- **Actuator**
 - **Spring MCP Server (Stdio/Webflux)**
 - **Lombok**: 1.18.38
 - **SpotBugs**: 4.8.6
@@ -16,3 +15,20 @@
 ```shell
 mvn clean package
 ```
+
+## Tools
+
+<div style="border: 1px solid #050505; padding: 10px; background-color: #0f1fb5;">
+<b>getStarWarsCharacter</b>
+    <ul> 
+      <li>Description: Gets Star Wars character information with ID
+      <li> Input: id (string): Character ID
+      <li> Output: String: Name, gender ve Date of birth
+    </ul>
+    <b>sum</b>
+    <ul> 
+      <li>Description: Sums two numbers
+      <li> Input: numberA (long): First number, numberB (long): Second number
+      <li> Output: long: Sum of numberA and numberB
+    </ul>
+</div>
--- src/main/java/com/thr/krdk/swapi/mcp/server/SwapiMcpStarterApplication.java
+++ src/main/java/com/thr/krdk/swapi/mcp/server/SwapiMcpStarterApplication.java
@@ -1,7 +1,10 @@
 package com.thr.krdk.swapi.mcp.server;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SwapiMcpStarterApplication {
@@ -9,4 +12,11 @@ public class SwapiMcpStarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwapiMcpStarterApplication.class, args);
     }
+
+    @Bean
+    public ToolCallbackProvider swapiTools(SwapiToolsService swapiToolsService, CalculatorToolsService calculatorToolsService) {
+        return MethodToolCallbackProvider.builder().toolObjects(swapiToolsService, calculatorToolsService).build();
+    }
+
+
 }
